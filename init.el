2ee38872401701951c2f766185c1a854832f2516
Justin Smestad <justin.smestad@gmail.com>
2018-01-28 11:46:57 -0700

    Switch base font

--- init.el
+++ init.el
@@ -175,7 +175,7 @@ It should only modify the values of Spacemacs settings."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Hack"
+   dotspacemacs-default-font '("Fira Mono"
                                :size 13.0
                                :weight normal
                                :width normal
